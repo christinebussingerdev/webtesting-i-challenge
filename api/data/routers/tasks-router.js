e5483fcf85cb13f805607269be832b72ffead7f2
Christine Bussinger <christinebussingerdev@gmail.com>
2020-07-20 20:27:40 -0400

    added task router api endpoints

---
+++ api/data/routers/tasks-router.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const db = require('./tasks-model')
+
+const tasks = express.Router()
+
+tasks.get('/api/tasks', async(req, res, next) => {
+  try{
+    const tasks = await db.find()
+
+    res.status(200).json(tasks)
+  }
+  catch(err) {next(err)}
+})
+
+tasks.post('/api/tasks', async(req, res, next) => {
+  try {
+    const newTask = await db.add(req.body)
+
+    res.status(201).json({msg: 'task created'})
+  }
+  catch(err) {next(err)}
+})
+
+tasks.delete('/api/tasks/:id', async(req, res, next) => {
+  try {
+    const deletedTask = await db.remove(req.params.id)
+    
+    res.status(204).json({msg: 'task deleted'})
+  }
+  catch(err) {next(err)}
+})
